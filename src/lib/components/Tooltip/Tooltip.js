0ee257b428279da68e10440e888f1778c7a80695
illia prokopchuk <illia.prokopchuk@globallogic.com>
2024-11-05 14:41:06 +0200

    Fix [Monitoring] missing tooltips on counters

--- src/lib/components/Tooltip/Tooltip.js
+++ src/lib/components/Tooltip/Tooltip.js
@@ -74,7 +74,8 @@ const Tooltip = ({
             : !child
               ? false
               : (child.nodeType !== Node.TEXT_NODE &&
-                  child.childNodes?.[0]?.nodeType !== Node.TEXT_NODE) ||
+                  (child.childNodes.length > 1 ||
+                    child.childNodes?.[0]?.nodeType !== Node.TEXT_NODE)) ||
                 /*
             If the child node is a text node and the text of the child node inside the container is greater than the width of the container, then show tooltip.
           */
