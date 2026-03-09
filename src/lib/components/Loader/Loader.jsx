5e60404cb96d016e8144d0ce7dfaf15fa4364c73
illia.prokopchuk <illia.prokopchuk@globallogic.com>
2026-03-09 15:43:19 +0200

    Multiple loader spinners appear sequentially when opening Nuclio UI

--- src/lib/components/Loader/Loader.jsx
+++ src/lib/components/Loader/Loader.jsx
@@ -31,9 +31,26 @@ const Loader = ({ secondary = false, section = false, small = false }) => {
     secondary && 'secondary-loader'
   )
 
+  const svgSize = small ? 20 : 40
+
   return (
     <div className={wrapperClassNames} data-testid="loader">
-      <div className="loader" />
+      <output aria-label="Loading" className="loader-output">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={svgSize}
+          height={svgSize}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="loader-icon"
+        >
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+      </output>
     </div>
   )
 }
